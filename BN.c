c34cb2c0fbbc62e8a8e80f2f157a5a05b3ad560c
DoroninaD <d.doronina@gmail.com>
2015-04-16 19:23:33 +0700

    Update BN.c

--- BN.c
+++ BN.c
@@ -1,60 +1,68 @@
-
 #include <stdio.h>
 #include <stdlib.h> 
 #include <memory.h>
 #include <string.h>
-#include <math.h>
-#include <string.h>
-#include <local.h>
-
+#include <locale.h>
 
 struct MyLong
 {
-	unsigned long long* pointer;
-	unsigned long long size;
+	unsigned int* pointer;
+	unsigned int size;
 
-	void SetMemory(unsigned long long s)
+	void SetMemory(unsigned int s)
 	{
 		size = s;
-		pointer = (unsigned long long*)malloc(sizeof(unsigned long long)*size);
-		memset(pointer, 0, sizeof(unsigned long long)*size);
+		pointer = (unsigned int*)malloc(sizeof(unsigned int)*size);
+		memset(pointer, 0, sizeof(unsigned int)*size);
 	}
 	
 	void Copy(MyLong a)
 	{
 		for (int i = 0; i < a.size; i++)
-		{
 			pointer[i] = a.pointer[i];
-		}
+	}
+
+	void FreeMemory()
+	{
+		free(pointer);
 	}
 };
 
+///Большие
+MyLong Sum(MyLong a, MyLong b);
+MyLong Sub(MyLong a, MyLong b);
+MyLong Mul(MyLong a, MyLong b);
+MyLong Divide(MyLong a, MyLong b, MyLong &mod);
+MyLong Pow(MyLong a, MyLong b, MyLong m); //использует smallPow, Pow32
+int Compare(MyLong a, MyLong b);
+
+///Маленькие
+MyLong ShortMul(MyLong a, unsigned int b);			
+MyLong smallPow(MyLong a, unsigned int p, MyLong m); //a^p mod m
+MyLong ShortDivide(MyLong a, unsigned int b);
+int ShortCompare(MyLong a, unsigned int b);
+
+//Вспомогательные
+MyLong Pow32(MyLong a, MyLong m);  //a^(2^32) mod m
+void ShiftLeft(MyLong &a, unsigned int i); //сдвиг на i элементов массива 
+MyLong Normalize(MyLong a);			//уменьшение размера a за счет убирания незначащих нулей
+
+//Работа с файлами
 MyLong ReadTextFile(char* file);
 MyLong ReadBinFile(char* file);
 bool WriteBinFile(char* file, MyLong number);
-bool WriteTextFile(char* file, MyLong number); 
-MyLong Summ(MyLong a, MyLong b);
-MyLong Sub(MyLong a, MyLong b);
-MyLong Mul(MyLong a, MyLong b);
-int Compare(MyLong a, MyLong b);
-int ShiftedCompare(MyLong a, MyLong b);
-MyLong Divide(MyLong a, MyLong b, MyLong &ost);
-MyLong Mod(MyLong a, MyLong b);
-MyLong smallPow(MyLong a,unsigned long long p, MyLong m);
-MyLong Pow(MyLong a, MyLong b, MyLong m);
-MyLong Pow32(MyLong a, MyLong m);
-MyLong Normalize(MyLong a);
-void ShiftedSub(MyLong a, MyLong b);
+bool WriteTextFile(char* file, MyLong number);
 
 
 int main(int argc, char * argv[])
 {
 	setlocale(LC_ALL,"Rus");
+
 	MyLong a;
 	MyLong b;
 	MyLong res;
 	MyLong m;
-
+	
 	if ( argc < 5 || argc > 7)
 	{
 		printf("Неверный формат строки!");
@@ -65,27 +73,24 @@ int main(int argc, char * argv[])
 
 	if (argv[argc - 1][1] == 'b' && argv[argc - 1][0] == '-')
 	{	
-
 		a = ReadBinFile(argv[1]);
 		if (a.size == 0)
 		{
-			free(a.pointer);
+			a.FreeMemory();
 			return 0;
 		}
-
 		b = ReadBinFile(argv[3]);
 		if (b.size == 0)
 		{
-			free(b.pointer);
+			b.FreeMemory();
 			return 0;
 		}
-
 		if (argv[2][0] == 'p')
 		{
 			m = ReadBinFile(argv[4]);
 			if (m.size == 0)
 			{
-				free(m.pointer);
+				m.FreeMemory();
 				return 0;
 			}
 		}
@@ -95,13 +100,13 @@ int main(int argc, char * argv[])
 		a = ReadTextFile(argv[1]);
 		if (a.size == 0)
 		{
-			free(a.pointer);
+			a.FreeMemory();
 			return 0;
 		}
 		b = ReadTextFile(argv[3]);
 		if (b.size == 0)
 		{
-			free(b.pointer);
+			b.FreeMemory();
 			return 0;
 		}
 		if (argv[2][0] == 'p')
@@ -109,12 +114,12 @@ int main(int argc, char * argv[])
 			m = ReadTextFile(argv[4]);
 			if (m.size == 0)
 			{
-				free(m.pointer);
+				m.FreeMemory();
 				return 0;
 			}
 		}
 	}
-
+	
 	if (strlen(argv[2])!=1)
 	{
 		printf("Ошибка ввода. Операция - один символ!");
@@ -124,7 +129,7 @@ int main(int argc, char * argv[])
 	{
 		switch (argv[2][0]) 
 		{
-		case '+': res = Summ(a, b);
+		case '+': res = Sum(a, b);
 			break;
 
 		case '-': res = Sub(a, b);
@@ -148,7 +153,6 @@ int main(int argc, char * argv[])
 			return 0;
 			break;
 		}
-
 	}
 
 	if (argv[argc - 1][1] == 'b' && argv[argc - 1][0] == '-')
@@ -177,6 +181,7 @@ int main(int argc, char * argv[])
 		int k = 4;
 		if (argv[2][0] == 'p')
 		{
+			
 			k = 5;
 		}
 		if (k < argc)
@@ -192,238 +197,210 @@ int main(int argc, char * argv[])
 			return 0;
 		}
 	}
-
 	printf("Операция выполнена успешно");
-
 	return 0;
 }
 
+void ShiftLeft(MyLong &a, unsigned int i) 
+{
+	MyLong a1;
 
+	a1.SetMemory(a.size + i);
+
+	for (int j = i; j < a.size + i; j++)
+		a1.pointer[j] = a.pointer[j - i];
+
+	a.FreeMemory();
+	a = a1;
+	a.size = a1.size;
+}
+int ShortCompare(MyLong a, unsigned int b)
+{
+	if (a.size > 1) return 1;
+	if (a.size == 0) return -1;
+	if (a.pointer[0] > b) return 1;
+	if (a.pointer[0] < b) return -1;
+	return 0;
+}
+
+MyLong Divide(MyLong a, MyLong b, MyLong &mod)
+{
+	MyLong res;
+	if (Compare(a,b) == -1)
+	{
+		res.SetMemory(1);
+		mod.SetMemory(a.size);
+		mod.Copy(a);
+	}
+	
+	res.SetMemory(a.size - b.size + 1);
+	MyLong A;
+	A.SetMemory(a.size);
+	A.Copy(a);
+
+	MyLong tmp;
+
+	int i = res.size - 1;
+	for(i = res.size - 1; i >-1; i--)
+	{
+			unsigned int x = 0;
+			unsigned long long low = 0;
+			unsigned long long up = 4294967295;
+			while (up - low >1)
+			{
+				unsigned long long d = (low + up) >> 1;
+				tmp = ShortMul(b, d);
+				ShiftLeft(tmp, i);
+				if (Compare(A,tmp)!= -1)
+				{
+					x = d;
+					low = d;
+				}
+				else up = d;
+				tmp.FreeMemory();
+			}
+			res.pointer[i] = x;
+			tmp = ShortMul(b, x);
+			ShiftLeft(tmp, i);
+			A = Sub(A, tmp);
+			tmp.FreeMemory();
+	}
+
+	mod = Normalize(A);
+	return Normalize(res);
+}
+MyLong ShortDivide(MyLong a, unsigned int b, unsigned int &ost)
+{
+	MyLong res;
+	if (!b)
+	{
+		printf("Деление на 0!");
+		res.SetMemory(1);
+		ost = 0;
+		res.size = 0;
+		return res;
+	}
+
+	if (ShortCompare(a, b) == -1)
+	{
+		ost = a.pointer[0];
+		res.SetMemory(1);
+		return res;
+	}
+
+	unsigned int s = (a.size - 1)*sizeof(int);
+
+	_asm
+	{
+			mov esi, a.pointer
+			add esi, s
+			mov ecx, a.size
+			xor edx, edx
+
+		m : 	mov eax, [esi]
+			div b
+			mov[esi], eax
+			add esi, 4
+			loop m
+			mov ost, edx
+	}
+
+	return Normalize(res);
+}
+MyLong ShortMul(MyLong a, unsigned int b)
+{
+	MyLong res;
+	res.SetMemory(a.size + 1);
+		_asm
+		{
+			mov esi, a.pointer
+			mov ecx, a.size
+			mov ebx, res.pointer
+
+		m:	mov eax, [esi]
+			mul b
+			add[ebx], eax
+			adc[ebx+4], edx
+			adc[ebx+8], 0
+			add esi,4
+			add ebx,4
+			loop m
+		}
+	return Normalize(res);
+}
+
+MyLong Mul(MyLong a, MyLong b)
+{
+	MyLong res;
+	res.SetMemory(a.size + b.size);
+	int cf = 0;
+	int bs = b.size * 4;
+	_asm
+	{
+			mov esi, a.pointer
+			mov ebx, res.pointer
+			mov ecx, a.size
+
+		m1 :	push ecx
+			mov edi, b.pointer
+			mov ecx, b.size
+		m2 :	mov eax, [esi]
+			mov edx, [edi]
+			mul edx
+			  
+			add[ebx], eax
+			adc edx, cf
+			jnc m3
+			mov cf, 1
+			jmp m4
+		m3 :	mov cf, 0
+		m4 :	add[ebx + 4], edx
+			jnc m5
+			mov cf, 1
+			
+		m5 :	add ebx, 4
+			add edi, 4
+				 
+			loop m2
+				 
+			pop ecx
+			sub ebx, bs
+
+			add esi, 4
+			add ebx, 4
+			loop m1
+	}
+
+	return Normalize(res);
+}
 
 int Compare(MyLong a, MyLong b) //-1:a<b 1:a>b 0:a=b 
 {
-	if (a.size<b.size)
-	{
-		return -1;
-	}
-	if ((a.size > b.size))
-	{
-		return 1;
-	}
+	if (a.size<b.size) return -1;
+	
+	if (a.size > b.size) return 1;
 
 	for (int i = a.size - 1; i > -1; i--)
 	{
-		if (a.pointer[i] > b.pointer[i])
-		{
-			return 1;
-		}
-		else if (a.pointer[i] < b.pointer[i])
-		{
-			return -1;
-		}
-	}
-	return 0;
-
-}
-
-
-bool ShiftLeft(MyLong a)
-{
-	int k = 0;
-	asm("movq  %0, %%rdi\n"
-		"movq  %1, %%rcx\n"
-		"clc\n"
-		"pushf\n"
-		"m : movq  (%%rdi), %%rax\n"
-		"popf\n"
-		"rcl  $1, %%rax\n"
-		"pushf\n"
-		"movq %%rax,(%%rdi) \n"
-		"addq $8,%%rdi \n"
-		"loop m\n"
-		"popf\n"
-		"jnc ex\n"
-		"movl $1, %k \n"
-		"ex:\n"
-		:
-		: "r" (a.pointer), "r" (a.size), "r"(k)
-		: "rdi", "rcx", "rax");
-
-	a = Normalize(a);
-	return k == 1;
-}
-void ShiftRight(MyLong a)
-{
-	unsigned long long tmp = (a.size - 1) * sizeof(unsigned long long);
-	asm(
-		"movq %0,%%rdi\n"
-		"addq %2,%%rdi\n"
-		"p:	 movq %1, %%rcx \n"
-		"clc\n"
-		"pushf\n"
-		"m : movq  (%%rdi), %%rax\n"
-		"popf\n"
-		"rcr $1, %%rax\n"
-		"pushf\n"
-		"movq  %%rax,(%%rdi)\n"
-		"subq $8, %%rdi \n"
-		"loop m\n"
-		"popf\n"
-		:
-		: "r"(a.pointer), "r"(a.size), "r"(tmp)
-		: "rdi", "rcx", "rax"
-		);
-}
-void ShiftedSub(MyLong a, MyLong b)
-{
-	unsigned long long tmp = (a.size - b.size)*sizeof(unsigned long long);
-
-	asm(
-		"clc\n"
-		"pushf\n"
-		"movq  %0, %%rsi\n"
-		"addq  %1, %%rsi\n"
-		"movq  %2, %%rdi\n"
-		"movq  %3, %%rcx\n"
-		"m : movq  (%%rsi), %%rax\n"
-		"popf\n"
-		"sbbq  (%%rdi), %%rax\n"
-		"pushf\n"
-		"movq %%rax, (%%rsi)\n"
-		"addq $8, %%rsi\n"
-		"addq $8, %%rdi\n"
-		"loop m\n"
-		"popf\n"
-		:
-		: "r"(a.pointer), "r"(tmp), "r"(b.pointer), "r"(b.size)
-		: "rsi", "rdi", "rcx", "rax"
-		);
-}
-int ShiftedCompare(MyLong a, MyLong b)
-{
-	for (size_t i = 0; i < b.size; i++)
-	{
-		if (a.pointer[a.size - 1 - i] > b.pointer[b.size - 1 - i])
-		{
-			return 1;
-		}
-		else if (a.pointer[a.size - 1 - i] < b.pointer[b.size - 1 - i])
-		{
-			return -1;
-		}
+		if (a.pointer[i] > b.pointer[i]) return 1;
+		if (a.pointer[i] < b.pointer[i]) return -1;	
 	}
 	return 0;
 }
+
 MyLong Normalize(MyLong a)
 {
-	while (a.pointer[a.size-1] == 0)
-	{
-		a.size--;
-	}
+	while (a.pointer[a.size - 1] == 0) a.size--;
+
 	if (a.size == 0) a.size = 1;
 
 	MyLong result;
 	result.SetMemory(a.size);
 	
-	for (size_t i = 0; i < a.size; i++)
-	{
-		result.pointer[i] = a.pointer[i];
-	}
+	for (size_t i = 0; i < a.size; i++) result.pointer[i] = a.pointer[i];
 
-	//free(a.pointer);
+	a.FreeMemory();
 	return result;
-}
-MyLong Divide(MyLong a, MyLong b, MyLong &ost)
-{
-	a = Normalize(a);
-	b = Normalize(b);
-
-	MyLong null;
-	null.SetMemory(1); 
-
-	if (Compare(a,b) == -1)
-	{
-		ost.SetMemory(a.size);	
-		for (int i = 0; i < ost.size; i++)
-		{
-			ost.pointer[i] = a.pointer[i];
-		}
-		return null;
-	}
-
-	MyLong result;
-	result.SetMemory(a.size - b.size + 1); 
-	MyLong A;
-	A.SetMemory(a.size);		
-	for (size_t i = 0; i < A.size; i++)
-	{
-		A.pointer[i] = a.pointer[i];
- 	}
-	MyLong B;
-	B.SetMemory(b.size+1); 
-	for (size_t i = 0; i < b.size; i++)
-	{
-		B.pointer[i+1] = b.pointer[i];
-	}
-
-	while ((B.pointer[B.size - 1] <= A.pointer[A.size - 1]) && (B.pointer[B.size - 1] & 0x8000000000000000) != 0x8000000000000000) //А что если вылезет?? Доделать
-	{
-		ShiftLeft(B);
-	}
-	if (B.pointer[B.size - 1] > A.pointer[A.size - 1])
-	{
-		ShiftRight(B);
-	}
-
-	while (Compare(A, b) != -1 || ShiftedCompare(B,b)!=-1)
-	{
-		if (ShiftedCompare(A, B) != -1)
-		{
-			result.pointer[0]++;
-			ShiftedSub(A, B);
-
-		}
-		ShiftLeft(result);
-		ShiftRight(B);
-
-
-		if (A.size == 1 && A.pointer[0] == 0 && B.pointer[0]==0)
-		{
-			for (int i = 0; i < B.size - 1; i++)
-			{
-				B.pointer[i] = B.pointer[i + 1];
-			}
-			B.pointer[B.size - 1] = 0;
-			B = Normalize(B);
-		}
-
-		if (B.pointer[B.size - 1] == 0 && A.pointer[A.size - 1] == 0)
-		{
-			A.size--;
-
-			if (A.size == B.size - 1)
-			{
-				B = Normalize(B);
-			}
-			else
-			{
-				for (unsigned long long i = B.size - 1; i > 0; i--)
-				{
-					B.pointer[i] = B.pointer[i - 1];
-				}
-				B.pointer[0] = 0;
-			}		
-		}
-	}
-	ShiftRight(result); 
-
-	ost = Normalize(A);
-
-	free(null.pointer);
-	free(A.pointer);
-	free(B.pointer);
-		
-	return Normalize(result);
 }
 MyLong Pow(MyLong a, MyLong p, MyLong m)
 {
@@ -434,17 +411,14 @@ MyLong Pow(MyLong a, MyLong p, MyLong m)
 	MyLong ost;
 	MyLong tmp;
 
-	if (Compare(a, m) != -1)
-	{
-		Divide(a, m, a);
-	}
+	if (Compare(a, m) != -1) Divide(a, m, a);
 
 	for (int i = p.size - 1; i > -1; i--)
 	{	
 		tmp = Divide(Mul(Pow32(res, m), smallPow(a, p.pointer[i], m)), m, res);
-		free(tmp.pointer);
+		tmp.FreeMemory();
 		tmp = Divide(Normalize(res), m, res);
-		free(tmp.pointer);
+		tmp.FreeMemory();
 	}
 	return res;
 }
@@ -456,23 +430,24 @@ MyLong Pow32(MyLong a, MyLong m)
 
 	MyLong tmp;
 
-	for (int i = 0; i < 64; i++)
+	for (int i = 0; i < 32; i++)
 	{
 		tmp = Divide(Mul(res, res), m, res);
-		free(tmp.pointer);
+		tmp.FreeMemory();
 	}
 	return res;
 }
-MyLong smallPow(MyLong a,unsigned long long p, MyLong m)
+MyLong smallPow(MyLong a,unsigned int p, MyLong m)
 {
 	MyLong res;
 	res.SetMemory(1);
 	res.pointer[0] = 1;
 
-	char c[64]; //двоичное представление числа p
+	char c[32]; //двоичное представление числа p
+	memset(c, 0, 32);
 	int k = 0; //определяет наибольший ненулевой элемент в c
 
-	for (int i = 0; i < 64; i++)
+	for (int i = 0; i < 32; i++)
 	{
 		c[i] = p % 2;
 		p /= 2;
@@ -486,68 +461,10 @@ MyLong smallPow(MyLong a,unsigned long long p, MyLong m)
 	for (int i = k; i >= 0; i--)
 	{
 		Divide(Mul(res, res), m, res);
-
-		if (c[i] == 1)
-		{
-			Divide(Mul(res, a), m, res);
-		}
+		if (c[i] == 1) Divide(Mul(res, a), m, res);
 	}
 	return res;
 }
-
-MyLong Mul(MyLong a, MyLong b)
-{
-	MyLong result;
-	result.SetMemory(a.size + b.size);
-
-	MyLong tmp;			
-	tmp.SetMemory(2);
-
-	unsigned long long carry = 0;
-
-	for (int i = 0; i < a.size; i++)
-	{
-		carry = 0;
-
-		for (int j = 0; j < b.size; j++)
-		{
-			int ik = i*sizeof(unsigned long long);
-			int jk = j*sizeof(unsigned long long);
-			//tmp = a.pointer[i] * b.pointer[j] + carry + result.pointer[i + j];
-			asm(
-				"movq %0, %%rsi\n"
-				"movq %1, %%rdi\n"
-				"addq %2, %%rsi\n"
-				"addq %3, %%rdi\n"
-				"movq %4, %%rbx\n"
-				"movq(%%rsi), %%rax\n"
-				"movq(%%rdi), %%rdx\n"
-				"mulq %%rdx\n"
-				"movq %%rax, (%%rbx)\n"
-				"movq %%rdx, (%%rbx + $8)\n"
-				"addq %6, (%%rbx)\n"
-				"adcq $0, (%%rbx + $8)\n"
-				"movq %5, %%rsi\n"
-				"addq %2, %%rsi\n"
-				"addq %3, %%rsi\n"
-				"movq(%%rsi), %%rax\n"
-				"addq %%rax, (%%rbx)\n"
-				"adcq $0, (%%rbx + $8)\n"
-				:
-				: "r"(a.pointer), "r"(b.pointer), "r"(ik), "r"(jk), "r"(tmp.pointer), "r"(result.pointer), "r"(carry)
-				: "rsi", "rdi", "rbx", "rax", "rdx", "rbx"
-				);
-
-			carry = tmp.pointer[1]; //carry = tmp / base;
-			result.pointer[i + j] = tmp.pointer[0];//result.pointer[i + j] = tmp%base;
-		}
-		result.pointer[i + b.size] = carry;
-	}
-	return Normalize(result);
-}
-
-
-
 MyLong Sub(MyLong a, MyLong b)
 {
 	MyLong maxnumber = a;
@@ -562,33 +479,31 @@ MyLong Sub(MyLong a, MyLong b)
 	result.SetMemory(maxnumber.size);
 	result.Copy(maxnumber);
 
-	
-	asm("clc\n"
-		"pushf\n"
-		"movq  %0, %%rsi\n"
-		"movq  %1, %%rdi\n"
-		"movq  %2, %%rcx\n"
-		"m : movq (%%rsi), %%rax\n"
-		"popf\n"
-		"sbbq(%%rdi), %%rax\n"
-		"pushf\n"
-		"movq %%rax, (%%rsi)\n"
-		"addq $8, %%rsi\n"
-		"addq $8, %%rdi\n"
-		"loop m\n"
-		"movq(%%rsi), %%rax\n"
-		"popf\n"
-		"sbbq $0, %%rax\n"
-		"movq %%rax, (%%rsi)\n"
-		:
-		: "r"(result.pointer), "r"(minnumber.pointer), "r"(minnumber.size)
-		: "rsi", "rdi", "rcx", "rax"
-		);
+	_asm
+	{
+			clc
+			pushf
+			mov esi, result.pointer
+			mov edi, minnumber.pointer
+			mov ecx, minnumber.size
+		m 	mov eax, [esi]
+			popf
+			sbb eax, [edi]
+			pushf
+			mov[esi], eax
+			add esi, 4
+			add edi, 4
+			loop m
+			mov eax, [esi]
+			popf
+			sbb eax, 0
+			mov[esi], eax
+	}
 	
 	return  Normalize(result);
 }
 
-MyLong Summ(MyLong a, MyLong b)
+MyLong Sum(MyLong a, MyLong b)
 {
 	MyLong maxnumber = a;
 	MyLong minnumber = b;
@@ -603,44 +518,41 @@ MyLong Summ(MyLong a, MyLong b)
 	result.SetMemory(maxnumber.size+1);
 
 	for (int i = 0; i < maxnumber.size; i++)
-	{
 		result.pointer[i] = maxnumber.pointer[i];
+
+	_asm
+	{
+		clc
+		pushf
+		mov esi, result.pointer
+		mov edi, minnumber.pointer
+		mov ecx, minnumber.size
+	m:	mov eax, [esi]
+		popf
+		adc eax, [edi]
+		pushf
+		mov  [esi], eax
+		add esi,4
+		add edi,4
+		loop m
+		mov eax, [esi]
+		popf
+		adc eax,0
+		mov  [esi],eax
+		jnc ex
+		dec result.size
+	ex:			
 	}
-
-	asm("clc\n"
-		"pushf\n"
-		"movq  %0, %%rsi\n"
-		"movq  %1, %%rdi\n"
-		"movq  %2, %%rcx\n"
-		"m : movq (%%rsi), %%rax\n"
-		"popf\n"
-		"adcq(%%rdi), %%rax\n"
-		"pushf\n"
-		"movq %%rax, (%%rsi)\n"
-		"addq $8, %%rsi\n"
-		"addq $8, %%rdi\n"
-		"loop m\n"
-		"movq(%%rsi), %%rax\n"
-		"popf\n"
-		"adcq $0, %%rax\n"
-		"movq %%rax, (%%rsi)\n"
-		:
-		: "r"(result.pointer), "r"(minnumber.pointer), "r"(minnumber.size)
-		: "rsi", "rdi", "rcx", "rax"
-		);
-
+	
 	return Normalize(result);
 }
-
 bool WriteBinFile(char* file, MyLong number)
 {
 	FILE* out;
 	if ((out = fopen(file,"wb")) != NULL)
 	{
 		for (int i = number.size - 1; i > -1; i--)
-		{
-			fwrite(&number.pointer[i], sizeof(unsigned long long*), 1, out);
-		}
+			fwrite(&number.pointer[i], sizeof(unsigned int*), 1, out);
 		fclose(out);
 		return true;
 	}
@@ -652,20 +564,15 @@ bool WriteBinFile(char* file, MyLong number)
 MyLong ReadTextFile(char* file)
 {
 	FILE* in;
-	
 	int size = 0;
-
 	MyLong res;
 	res.SetMemory(1);
 
 	if ((in = fopen(file, "r")) != NULL)
 	{
-		while (fgetc(in) != EOF)
-		{
-			size++;
-		}
+		while (fgetc(in) != EOF) size++;
 
-		if (size == 0)
+		if (!size)
 		{
 			res.SetMemory(1);
 			res.size = 0;
@@ -679,10 +586,14 @@ MyLong ReadTextFile(char* file)
 		MyLong k;
 		k.SetMemory(1);
 		k.pointer[0] = 1;
+
+		MyLong tmp1;
 		
 		for (int l = 0; l < size; l++)
-		{	
-			k = Normalize(Mul(k, c));
+		{				
+			tmp1 = Mul(k, c);
+			k.FreeMemory();
+			k = tmp1;
 		}
 
 		MyLong tmp;
@@ -691,27 +602,35 @@ MyLong ReadTextFile(char* file)
 		MyLong ost;
 		ost.SetMemory(1);
 		char ch;
-		unsigned long long h = 0;
+		unsigned int h = 0;
 		while ((ch = fgetc(in))!=EOF)
 		{
-			k = Divide(k, c, ost);
-			free(ost.pointer);
-			tmp.pointer[0] = ch - '0';
-			res = Summ(res, Normalize(Mul(tmp, k)));	
-		}
+			if ((ch - '0' > 9) || (ch - '0' < 0))
+			{
+				printf("Файл %s содержит не только цифры!");
+				res.size = 0;
+				return res;
+			}
 
+			k = Divide(k, c, ost);
+
+			ost.FreeMemory();
+
+			tmp.pointer[0] = ch - '0';
+			res = Sum(res, Normalize(Mul(tmp, k)));	
+		}
 		
 		fclose(in);
 
-		free(k.pointer);
-		free(c.pointer);
-		free(tmp.pointer);
+		k.FreeMemory();
+		c.FreeMemory();
+		tmp.FreeMemory();
+
 	}
 	else
 	{
 		printf("Не удалось открыть файл %s!", file);
-		res.SetMemory(1);
-		res.size = 0;	
+		res.size = 0;		
 	}
 
 	return res;
@@ -736,22 +655,20 @@ bool WriteTextFile(char* file, MyLong number)
 
 	//считаем количество элементов выходного массива
 	MyLong ost;
-	unsigned long long k = 1;
+	unsigned int k = 1;
 	while (Compare(numcopy, null) == 1)
 	{
 		numcopy = Divide(numcopy, dec, ost);
 		k++;
 	}
-	if (k!=1)
-	{
-		k--;
-	}
+	if (k != 1) k--;
 	MyLong res;
 	res.SetMemory(k);
 	
 	int i = 0;
 	while (Compare(number, null) == 1)
 	{
+		
 		number = Divide(number, dec, ost);
 		res.pointer[i] = ost.pointer[0];
 		i++;
@@ -778,18 +695,22 @@ bool WriteTextFile(char* file, MyLong number)
 			}
 		}
 		fclose(out);
+		numcopy.FreeMemory();
+		dec.FreeMemory();
+		tmp.FreeMemory();
+		null.FreeMemory();
+		res.FreeMemory();
+		
 		return true;
 	}
-
+	numcopy.FreeMemory();
+	dec.FreeMemory();
+	tmp.FreeMemory();
+	null.FreeMemory();
+	res.FreeMemory();
 		printf("Не удалось открыть файл %s!", file);
 		return false;
 
-	free(numcopy.pointer);
-	free(dec.pointer);
-	free(tmp.pointer);
-	free(null.pointer);
-	free(res.pointer);
-	
 } 
 MyLong ReadBinFile(char* file)
 {
@@ -799,10 +720,7 @@ MyLong ReadBinFile(char* file)
 	
 	if ((in = fopen(file, "rb")) != NULL)
 	{
-		while (fgetc(in) != EOF)
-		{
-			size++;
-		}
+		while (fgetc(in) != EOF) size++;
 
 		if (size == 0)
 		{
@@ -812,15 +730,12 @@ MyLong ReadBinFile(char* file)
 			return number;
 		}
 
-		number.SetMemory((size - 1) / sizeof(unsigned long long)+1);
+		number.SetMemory((size - 1) / sizeof(unsigned int)+1);
 
 		fseek(in, 0, 0);
 
 		for (int i = number.size - 1; i >-1; i--)
-		{
-			fread(&number.pointer[i], sizeof(unsigned long long), 1, in);
-		}
-
+			fread(&number.pointer[i], sizeof(unsigned int), 1, in);
 		fclose(in);
 	}
 	else
@@ -831,6 +746,3 @@ MyLong ReadBinFile(char* file)
 	}
 	return number;
 }
-	
-
-
