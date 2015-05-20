7f50711ee0556d45742e2c33f13773629b221235
DoroninaD <d.doronina@gmail.com>
2015-05-20 23:54:12 +0700

    Update long.h

--- func/long.h
+++ func/long.h
@@ -10,15 +10,13 @@ void SetMemory(unsigned long long s, MyLong *p);
 void Copy(MyLong a, MyLong *p);
 void FreeMemory(MyLong *p);
 
-///Большие
 MyLong Sum(MyLong a, MyLong b);
 MyLong Sub(MyLong a, MyLong b);
 MyLong Mul(MyLong a, MyLong b);
 MyLong Divide(MyLong a, MyLong b, MyLong *mod);
-MyLong Pow(MyLong a, MyLong b, MyLong m); //использует smallPow, Pow32
+MyLong Pow(MyLong a, MyLong b, MyLong m); 
 int Compare(MyLong a, MyLong b);
 
-///Маленькие
 MyLong ShortSum(MyLong a, unsigned long long b);
 MyLong ShortSub(MyLong a, unsigned long long b);
 MyLong ShortMul(MyLong a, unsigned long long b);
@@ -26,12 +24,10 @@ MyLong ShortDivide(MyLong a, unsigned long long b, unsigned long long *ost);
 MyLong smallPow(MyLong a, unsigned long long p, MyLong m); //a^p mod m
 int ShortCompare(MyLong a, unsigned long long b);
 
-//Вспомогательные
 MyLong Pow64(MyLong a, MyLong m);  //a^(2^32) mod m
-MyLong ShiftLeft(MyLong *a, unsigned long long i); //сдвиг на i элементов массива 
-MyLong Normalize(MyLong *a);			//уменьшение размера a за счет убирания незначащих нулей
+MyLong ShiftLeft(MyLong *a, unsigned long long i); 
+MyLong Normalize(MyLong *a);			
 
-//Работа с файлами
 char* toString(MyLong number);
 MyLong ReadFromString(char* buffer);
 MyLong ReadTextFile(char* file);
